0aa75dbb6110ccdd3d51b670b18cd50eb3fc72f1
Thomas Brown <thomasbrwn22@gmail.com>
2018-11-13 19:53:35 +0000

    Commit.

--- src/gap_buffer.hpp
+++ src/gap_buffer.hpp
@@ -1,20 +1,126 @@
 #pragma once
 
+#include <stdlib.h>
+#include <string.h>
+
+template <typename T>
 struct GapBuffer {
-    GapBuffer();
+    GapBuffer(int min_size);
     ~GapBuffer();
 
     void resize_gap(size_t new_sz);
-    void insert_at_gap(char c);
-    void insert_at_gap(char *str);
+    void insert_at_gap(T c);
+    void insert_at_gap(T *arr);
     void remove_from_back();
     bool remove_at_gap();
     bool move_gap_left();
     bool move_gap_right();
 
-    char *data;
+    T *data;
     int gap_start;
     int gap_end;
     size_t sz;
+    size_t MIN_SIZE;
     size_t BUFFER_GROWBY;
 };
+
+template <typename T>
+GapBuffer<T>::GapBuffer(int min_size)
+{
+    MIN_SIZE = min_size;
+    BUFFER_GROWBY = 10;
+    sz = BUFFER_GROWBY;
+    data = (T*)malloc(sz + 1);
+    data[sz] = '\0';
+    memset(data, 0, sz - 1);
+    gap_start = 0;
+    gap_end = sz;
+}
+
+template <typename T>
+GapBuffer<T>::~GapBuffer()
+{
+    free(data);
+}
+
+template <typename T>
+void GapBuffer<T>::resize_gap(size_t new_sz)
+{
+    T *new_data = (T*)malloc((new_sz + 1) * sizeof(T));
+    new_data[new_sz] = '\0';
+    memset(new_data, 0, new_sz - 1);
+    memcpy(new_data, data, gap_start);
+    memcpy(new_data + gap_end + BUFFER_GROWBY, data + gap_end, sz - gap_end);
+    
+    free(data);
+
+    gap_end += BUFFER_GROWBY;
+    sz = new_sz;
+    data = new_data;
+}
+
+template <typename T>
+void GapBuffer<T>::insert_at_gap(T c)
+{
+    data[gap_start++] = c;
+
+    if (gap_start == gap_end)
+    {
+        resize_gap(sz + BUFFER_GROWBY);
+    }
+}
+
+template <typename T>
+void GapBuffer<T>::insert_at_gap(T *arr)
+{
+    for (; *arr; ++arr)
+    {
+        insert_at_gap(*arr);
+    }
+}
+
+template <typename T>
+void GapBuffer<T>::remove_from_back()
+{
+    if (gap_end < sz)
+        ++gap_end;
+}
+
+template <typename T>
+bool GapBuffer<T>::remove_at_gap()
+{
+    if (gap_start > MIN_SIZE && gap_start > 0)
+    {
+        --gap_start;
+        
+        return true;
+    }
+
+    return false;
+}
+
+template <typename T>
+bool GapBuffer<T>::move_gap_left()
+{
+    if (gap_start > 0)
+    {
+        data[--gap_end] = data[--gap_start];
+
+        return true;
+    }
+
+    return false;
+}
+
+template <typename T>
+bool GapBuffer<T>::move_gap_right()
+{
+    if (gap_end < sz)
+    {
+        data[gap_start++] = data[gap_end++];
+    
+        return true;
+    }
+
+    return false;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,53 +8,101 @@ int main(int argc, char *argv[])
 {
     Console *app = new Console("Flash", 640, 480);
 
-    GapBuffer *line = new GapBuffer;
+    GapBuffer<GapBuffer<char>*> *file = new GapBuffer<GapBuffer<char>*>(1);
+    file->insert_at_gap(new GapBuffer<char>(0));
+    GapBuffer<char> *cur_line = file->data[0];
 
     while (!app->window_should_close())
     {
         app->poll_events();
 
+        if (app->is_key_pressed(SDLK_RETURN))
+        {
+            file->insert_at_gap(new GapBuffer<char>(0));
+            cur_line = file->data[file->gap_start - 1];
+            ++app->cursor_line.y;
+            app->cursor_line.x = cur_line->sz - (cur_line->gap_end - cur_line->gap_start);
+        }
         if (app->is_key_pressed(SDLK_UP))
         {
-            //--app->cursor_line.y;
+            if (file->move_gap_left())
+            {
+                cur_line = file->data[file->gap_start - 1];
+                --app->cursor_line.y;
+                size_t length = cur_line->sz - (cur_line->gap_end - cur_line->gap_start);
+                app->cursor_line.x = length;
+                for (int i = 0; i < length; i++)
+                    cur_line->move_gap_right();
+            }
         }
         else if (app->is_key_pressed(SDLK_DOWN))
         {
-            //++app->cursor_line.y;
+            if (file->move_gap_right())
+            {
+                cur_line = file->data[file->gap_start - 1];
+                ++app->cursor_line.y;
+                app->cursor_line.x = cur_line->sz - (cur_line->gap_end - cur_line->gap_start);
+            }
         }
         else if (app->is_key_pressed(SDLK_LEFT))
         {
-            if (line->move_gap_left())
+            if (cur_line->move_gap_left())
                 --app->cursor_line.x;
         }
         else if (app->is_key_pressed(SDLK_RIGHT))
         {
-            if (line->move_gap_right())
+            if (cur_line->move_gap_right())
                 ++app->cursor_line.x;
         }
         else if (app->is_key_pressed(SDLK_BACKSPACE))
         {
-            if (line->remove_at_gap())
+            if (cur_line->remove_at_gap())
                 --app->cursor_line.x;
+            else if (cur_line->gap_end + cur_line->gap_start == cur_line->sz)
+            {
+                if (file->remove_at_gap())
+                {
+                    cur_line = file->data[file->gap_start - 1];                 
+                    --app->cursor_line.y;
+                    app->cursor_line.x = cur_line->sz - (cur_line->gap_end - cur_line->gap_start); 
+                }
+                else
+                {
+                    file->move_gap_left();
+                    file->remove_at_gap();
+                    cur_line = file->data[file->gap_start - 1];                 
+                    --app->cursor_line.y;
+                }
+            }
         }
         else if (app->is_key_pressed(SDLK_DELETE))
         {
-            line->remove_from_back();
+            cur_line->remove_from_back();
         }
 
         if (app->c)
         {
-            line->insert_at_gap(app->c);
+            cur_line->insert_at_gap(app->c);
             ++app->cursor_line.x;
         }
 
         app->clear();
 
-        int j = 0;
-        for (int i = 0; i < line->sz; i++)
+        int l = 0;
+        for (int k = 0; k < file->sz; ++k)
         {
-            if (i < line->gap_start || i >= line->gap_end)
-                app->mvputch(line->data[i], j++, 0);
+            if (k < file->gap_start || k >= file->gap_end)
+            {
+                GapBuffer<char> *line = file->data[k];
+                int j = 0;
+                for (int i = 0; i < line->sz; ++i)
+                {
+                    if (i < line->gap_start || i >= line->gap_end)
+                        app->mvputch(line->data[i], j++, l);
+                }
+
+                l++;
+            }
         }
 
         app->draw_cursor();
